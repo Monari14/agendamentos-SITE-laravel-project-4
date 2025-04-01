86db0b198f76ac1f7fe25b73e85ac827369bba3e
Felipe Monari <133614580+Monari14@users.noreply.github.com>
2025-04-01 20:39:30 -0300

    chore: tabelas estão em migrations pra ficar mais facil pro monari do futuro 👍

--- backend/resources/views/dashboard/dash.blade.php
+++ backend/resources/views/dashboard/dash.blade.php
@@ -6,17 +6,8 @@
     <div class="container">
     <h2>Bem vindo(a) à Dashboard, @ {{ $username }}!</h2>
     <br><br>
-    @if ($agendamentos->isNotEmpty())
         <p>
-            Você tem {{ $agendamentos->count() }}
-            {{ Str::plural('agendamento', $agendamentos->count()) }}.
-            <a href="/dashboard/meus-agendamentos">Ver agora</a>.
-        </p>
-    @else
-        <p>
-            Nenhum agendamento encontrado.
             <a href="/dashboard/novo-agendamento">Faça um agendamento agora</a>.
         </p>
-    @endif
     </div>
 @endsection
